76a1e0dfa281ab1c9417a90ee804e2d74a183996
Aaron Huff <aaron.t.huf@gmail.com>
2023-04-27 18:27:16 +0000

    addpagination and styling

--- script.js
+++ script.js
@@ -1,35 +1,88 @@
-fetch('data/traits_filters.json')
+let images = [];
+let currentPage = 1;
+let imagesPerPage = 6;
+
+// Load filters from JSON file
+fetch('data/filters.json')
     .then(response => response.json())
-    .then(data => {
-        // code to display images based on filter categories
+    .then(filters => {
+        // Create a button for each filter
+        const buttonsDiv = document.getElementById('buttons');
+        filters.traits.forEach(filter => {
+            const button = document.createElement('button');
+            button.textContent = filter.name;
+            button.className = 'btn btn-light btn-sm';
+            button.type = 'button';
+            button.addEventListener('click', () => {
+                displayImages(filter);
+            });
+            buttonsDiv.appendChild(button);
+        });
     });
 
+function displayImages(filter) {
+    // Load images for selected filter
+    images = filter.images;
 
-function filterImages(category) {
-    const images = document.querySelectorAll("#image-container img");
-    images.forEach((img) => {
-        const categoryData = data.categories.find(c => c.name === category);
-        if (category === "All" || categoryData.images.includes(img.src.split('/').pop())) {
-            img.style.display = "block";
+    // Show first page of images and render pagination buttons
+    currentPage = 1;
+    showPage(currentPage);
+    renderPagination();
+}
+
+
+// Show a specific page of images
+function showPage(page) {
+    const startIndex = (page - 1) * imagesPerPage;
+    const endIndex = startIndex + imagesPerPage;
+    const pageImages = images.slice(startIndex, endIndex);
+
+    const pagesDiv = document.getElementById('pages');
+    while (pagesDiv.firstChild) {
+        pagesDiv.removeChild(pagesDiv.firstChild);
+    }
+    const pageDiv = document.createElement('div');
+    pageDiv.classList.add('page');
+
+    pageImages.forEach((image, index) => {
+        const img = document.createElement('img');
+        img.className = 'image rounded';
+        img.src = image;
+        img.alt = image.split('.')[0];
+        img.id = `image-${startIndex + index}`;
+        pageDiv.appendChild(img);
+    });
+
+    pagesDiv.appendChild(pageDiv);
+    setActivePage(page);
+}
+
+// Render pagination buttons
+function renderPagination() {
+    const paginationDiv = document.querySelector('.pagination');
+    paginationDiv.innerHTML = '';
+
+    const pageCount = Math.ceil(images.length / imagesPerPage);
+
+    for (let i = 1; i <= pageCount; i++) {
+        const button = document.createElement('button');
+        button.textContent = i;
+        button.className = 'btn btn-light btn-sm';
+        button.addEventListener('click', () => {
+            showPage(i);
+        });
+        paginationDiv.appendChild(button);
+    }
+}
+
+// Set active page button
+function setActivePage(page) {
+    const buttons = document.querySelectorAll('.pagination button');
+    buttons.forEach(button => {
+        if (parseInt(button.textContent) === page) {
+            button.classList.add('active');
         } else {
-            img.style.display = "none";
+            button.classList.remove('active');
         }
     });
 }
-
-const filterBtns = document.querySelectorAll(".filter-btn");
-filterBtns.forEach((btn) => {
-    btn.addEventListener("click", () => {
-        filterImages(btn.dataset.category);
-    });
-});
-
-filterImages("all");
-
-data.traits.forEach((category) => {
-    const button = document.createElement("button");
-    button.classList.add("filter-btn");
-    button.dataset.category = category.name;
-    button.innerText = category.name;
-    document.getElementById("filter-buttons").appendChild(button);
-  });
--- scripts/compress_images.py
+++ scripts/compress_images.py
@@ -12,7 +12,8 @@ logging.info("Compressing images and saving to images/")
 for image in images:
     bear = image.split("/")[-1]
     logging.info("Compressing {}".format(bear))
-    new_path = "images/thumbnails/" + image.split("/")[-1]
+    new_path = "images/thumbnails/" + image.split("/")[-1][11:]
+    # truncated to just bear number
     img = Image.open(image)
     img.thumbnail((400, 400))
     img.save(new_path, "PNG")
